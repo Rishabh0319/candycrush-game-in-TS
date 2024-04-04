f09ad127c4c33b95ee3d04789ad6b779d62ae667
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-04-04 16:13:53 +0530

    swaping of Tile functionality is done and swaping on adjacent (TOP,RIGHT,BOTTOM,LEFT) only feature also implemented while Tile swaping

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,9 @@ let candies = ['Blue', 'Orange', 'Green', 'Yellow', 'Red', 'Purple'];
 let rows = 9;
 let columns = 9;
 let board = [];
+let sourceTile;
+let destinationTile;
+// load game on browser
 window.onload = startGame;
 function startGame() {
     loadRandomCandiesOnBoard();
@@ -20,10 +23,61 @@ function loadRandomCandiesOnBoard() {
         for (let c = 0; c < columns; c++) {
             let tile = new Image();
             tile.id = `${r}-${c}`;
-            tile.src = `../src/assets/images/${randomCandy()}.png`;
+            tile.src = `./assets/images/${randomCandy()}.png`;
             row.push(tile);
             (_a = document.querySelector("#board")) === null || _a === void 0 ? void 0 : _a.appendChild(tile);
+            // Drag and Drop Events
+            tile.addEventListener('dragstart', dragStart);
+            tile.addEventListener('drop', dragDrop);
+            tile.addEventListener('dragend', dragEnd);
+            // prevent default events
+            tile.addEventListener('dragover', dragOver);
+            tile.addEventListener('dragleave', dragLeave);
+            tile.addEventListener('dragenter', dragEnter);
         }
         board.push(row);
     }
+    console.log(board);
+}
+// Defining Event Handler functions (Drag & Drop)
+function dragStart() {
+    // get the source Tile Referance
+    sourceTile = this;
+}
+function dragDrop() {
+    // get the destination Tile Referance
+    destinationTile = this;
+}
+function dragEnd() {
+    // swap the tile images
+    swapTiles(sourceTile, destinationTile);
+}
+function swapTiles(sourceTile, destinationTile) {
+    let sourceTileImage = sourceTile.src;
+    let destinationTileImage = destinationTile.src;
+    // get the [row][column] of source and destination Tile
+    let sr = Number(sourceTile.id.split('-')[0]);
+    let sc = Number(sourceTile.id.split('-')[1]);
+    let dr = Number(destinationTile.id.split('-')[0]);
+    let dc = Number(destinationTile.id.split('-')[1]);
+    // Only swap tile on Adjacent (TOP,DOWN,LEFT,RIGHT)
+    let top = sc === dc && sr === dr + 1;
+    let bottom = sc === dc && sr === dr - 1;
+    let left = sr === dr && sc === dc + 1;
+    let right = sr === dr && sc === dc - 1;
+    // swaping Tiles
+    if (top || bottom || left || right) {
+        sourceTile.src = destinationTileImage;
+        destinationTile.src = sourceTileImage;
+    }
+}
+// prevent Default Events
+function dragOver(event) {
+    event.preventDefault();
+}
+function dragLeave(event) {
+    event.preventDefault();
+}
+function dragEnter(event) {
+    event.preventDefault();
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ let rows: number = 9;
 let columns: number = 9;
 let board: HTMLImageElement[][] = [];
 
+let sourceTile: HTMLImageElement;
+let destinationTile: HTMLImageElement;
+
+// load game on browser
 window.onload = startGame;
 
 function startGame() {
@@ -23,10 +27,73 @@ function loadRandomCandiesOnBoard() {
         for (let c = 0; c < columns; c++) {
             let tile: HTMLImageElement = new Image();
             tile.id = `${r}-${c}`;
-            tile.src = `../src/assets/images/${randomCandy()}.png`;
+            tile.src = `./assets/images/${randomCandy()}.png`;
             row.push(tile);
             document.querySelector("#board")?.appendChild(tile);
+
+            // Drag and Drop Events
+            tile.addEventListener('dragstart', dragStart);
+            tile.addEventListener('drop', dragDrop);
+            tile.addEventListener('dragend', dragEnd);
+
+            // prevent default events
+
+            tile.addEventListener('dragover', dragOver);
+            tile.addEventListener('dragleave', dragLeave);
+            tile.addEventListener('dragenter', dragEnter);
         }
         board.push(row);
     }
+    console.log(board);
+}
+
+// Defining Event Handler functions (Drag & Drop)
+
+function dragStart(this: HTMLImageElement) {
+    // get the source Tile Referance
+    sourceTile = this;
+}
+
+function dragDrop(this: HTMLImageElement) {
+    // get the destination Tile Referance
+    destinationTile = this;
+}
+
+function dragEnd(this: HTMLImageElement) {
+    // swap the tile images
+    swapTiles(sourceTile, destinationTile);
+}
+
+function swapTiles(sourceTile: HTMLImageElement, destinationTile: HTMLImageElement) {
+    let sourceTileImage: string = sourceTile.src;
+    let destinationTileImage: string = destinationTile.src;
+
+    // get the [row][column] of source and destination Tile
+    let sr: number = Number(sourceTile.id.split('-')[0]);
+    let sc: number = Number(sourceTile.id.split('-')[1]);
+    let dr: number = Number(destinationTile.id.split('-')[0]);
+    let dc: number = Number(destinationTile.id.split('-')[1]);
+
+    // Only swap tile on Adjacent (TOP,DOWN,LEFT,RIGHT)
+    let top: boolean = sc === dc && sr === dr + 1;
+    let bottom: boolean = sc === dc && sr === dr - 1;
+    let left: boolean = sr === dr && sc === dc + 1;
+    let right: boolean = sr === dr && sc === dc - 1;
+
+    // swaping Tiles
+    if (top || bottom || left || right) {
+        sourceTile.src = destinationTileImage;
+        destinationTile.src = sourceTileImage;
+    }
+}
+
+// prevent Default Events
+function dragOver(event: DragEvent) {
+    event.preventDefault();
+}
+function dragLeave(event: DragEvent) {
+    event.preventDefault();
+}
+function dragEnter(event: DragEvent) {
+    event.preventDefault();
 }
